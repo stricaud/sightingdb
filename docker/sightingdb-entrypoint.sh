8520f87204446f42c357a8badbf7f17187f673c9
Sebastien Tricaud <sebastien@honeynet.org>
2020-04-29 16:30:26 -0700

    Leverage the -k option to set the key upon startup

--- docker/sightingdb-entrypoint.sh
+++ docker/sightingdb-entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-/opt/sightingdb/target/release/sightingdb
 if [ -z $SIGHTINGDB_APIKEY ]
 then
     echo "The environment variable SIGHTINGDB_API is not set, so we leave the default to 'changeme'."
+    /opt/sightingdb/target/release/sightingdb
 else
-    curl -k -H "Authorization: changeme" "https://localhost:9999/w/_config/acl/apikeys/$SIGHTINGDB_APIKEY?val="""
-    curl -k -H "Authorization: $SIGHTINGDB_APIKEY" "https://localhost:9999/d/_config/acl/apikeys/changeme?val="""
+    /opt/sightingdb/target/release/sightingdb -k $SIGHTINGDB_APIKEY    
 fi
-
-sightingdb_pid=$(ps |grep [s]ightingdb |cut -d' ' -f3)
-wait $sightingdb_pid
-
